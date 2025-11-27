5857ca994c52f8ef7266ce9d26c7b2e49a227601
Aswun <aswinlutfian32@gmail.com>
2025-11-27 12:08:03 +0700

    small fix

--- app/src/main/java/com/example/prak9/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/prak9/view/route/DestinasiEntry.kt
@@ -1,6 +1,8 @@
 package com.example.prak9.view.route
 
+import com.example.prak9.R
+
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
-    override val titleRes = "Tambah Siswa"
+    override val titleRes = R.string.entry_siswa
 }
--- app/src/main/java/com/example/prak9/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/prak9/view/route/DestinasiHome.kt
@@ -1,6 +1,7 @@
 package com.example.prak9.view.route
 
+import com.example.prak9.R
 object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
-    override val titleRes = "Data Siswa"
+    override val titleRes = R.string.app_name
 }
