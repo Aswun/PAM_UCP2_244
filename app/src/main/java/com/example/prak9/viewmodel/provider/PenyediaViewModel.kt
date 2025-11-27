5d991592f77f269e261952506289ef267b178bc5
Aswun <aswinlutfian32@gmail.com>
2025-11-27 15:23:05 +0700

    membuat PenyediaViewModel.kt pada package viewmodel.provider

--- app/src/main/java/com/example/prak9/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/prak9/viewmodel/provider/PenyediaViewModel.kt
@@ -1,5 +1,35 @@
 package com.example.prak9.viewmodel.provider
 
-class PenyediaViewModel {
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.prak9.repo.AplikasiSiswa
+import com.example.prak9.viewmodel.EntryViewModel
+import com.example.prak9.viewmodel.HomeViewModel
 
+/**
+ * Menyediakan objek [ViewModelProvider.Factory] untuk pembuatan instance [HomeViewModel] dan [EntryViewModel]
+ */
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        // Initializer untuk HomeViewModel
+        initializer {
+            HomeViewModel(
+                aplikasiSiswa().container.repoSiswa
+            )
+        }
+        // Initializer untuk EntryViewModel
+        initializer {
+            EntryViewModel(
+                aplikasiSiswa().container.repoSiswa
+            )
+        }
+    }
 }
+
+/**
+ * Fungsi ekstensi untuk mendapatkan instance [AplikasiSiswa] dari [CreationExtras]
+ */
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
