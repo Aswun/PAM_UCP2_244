6e93dfbb4b6caf2e73ef94f64898ddac855b4584
Aswun <aswinlutfian32@gmail.com>
2025-12-04 14:30:14 +0700

    menambahkan email pada EntryViewModel.kt

--- app/src/main/java/com/example/prak9/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/prak9/viewmodel/EntryViewModel.kt
@@ -54,6 +54,7 @@ data class DetailSiswa(
     val id: Int = 0,
     val nama: String = "",
     val alamat: String = "",
+    val email: String = "",
     val telpon: String = ""
 )
 
@@ -62,6 +63,7 @@ fun DetailSiswa.toSiswa(): Siswa = Siswa(
     id = id,
     nama = nama,
     alamat = alamat,
+    email = email,
     telpon = telpon
 )
 
@@ -70,6 +72,7 @@ fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
     id = id,
     nama = nama,
     alamat = alamat,
+    email = email,
     telpon = telpon
 )
 
