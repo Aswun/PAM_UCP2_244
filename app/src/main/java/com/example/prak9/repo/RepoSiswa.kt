b77b699950f2dbf19a0ab2b30ffa89273430a5e5
Aswun <aswinlutfian32@gmail.com>
2025-12-04 14:03:18 +0700

    menambahkan fungsi delete dan update pada RepoSiswa.kt

--- app/src/main/java/com/example/prak9/repo/RepoSiswa.kt
+++ app/src/main/java/com/example/prak9/repo/RepoSiswa.kt
@@ -9,6 +9,8 @@ interface RepoSiswa {
     suspend fun insertSiswa(siswa: Siswa)
 
     fun getSiswaStream(id: Int): Flow<Siswa?>
+    suspend fun deleteSiswa(siswa: Siswa)
+    suspend fun updateSiswa(siswa: Siswa)
 }
 
 class OfflineRepoSiswa(
@@ -17,5 +19,7 @@ class OfflineRepoSiswa(
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
     override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
 }
 
